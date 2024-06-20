8131e64fe838ba42c9357cf85b0b19e72d2db7a9
scossar <scossar3@gmail.com>
2024-06-20 16:34:03 -0700

    refactor

--- lib/utils/discourse_config.rb
+++ lib/utils/discourse_config.rb
@@ -13,7 +13,9 @@ module Discourse
         def check_config
           discourse_username = Discourse::Config.get('credentials', 'discourse_username')
 
-          discourse_username ||= CLI::UI::Prompt.ask("What's your Discourse username?")
+          return if discourse_username
+
+          discourse_username = CLI::UI::Prompt.ask("What's your Discourse username?")
           Discourse::Config.set('credentials', 'discourse_username', discourse_username)
         end
       end
