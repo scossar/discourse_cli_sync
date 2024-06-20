94cb82a2357ec9c5b20bb5b5bd8e62e66960f004
scossar <scossar3@gmail.com>
2024-06-20 16:27:09 -0700

    use class << self

--- lib/utils/discourse_config.rb
+++ lib/utils/discourse_config.rb
@@ -3,15 +3,19 @@
 module Discourse
   module Utils
     class DiscourseConfig
-      def self.call
-        check_config
-      end
+      class << self
+        def call
+          check_config
+        end
 
-      def self.check_config
-        discourse_username = Discourse::Config.get('credentials', 'discourse_username')
+        private
 
-        discourse_username ||= CLI::UI::Prompt.ask("What's your Discourse username?")
-        Discourse::Config.set('credentials', 'discourse_username', discourse_username)
+        def check_config
+          discourse_username = Discourse::Config.get('credentials', 'discourse_username')
+
+          discourse_username ||= CLI::UI::Prompt.ask("What's your Discourse username?")
+          Discourse::Config.set('credentials', 'discourse_username', discourse_username)
+        end
       end
     end
   end
