217976733f5f6a3843c81cc2111d089285cd86a5
scossar <scossar3@gmail.com>
2024-06-20 20:32:23 -0700

    reformat

--- lib/utils/discourse_config.rb
+++ lib/utils/discourse_config.rb
@@ -5,18 +5,28 @@ module Discourse
     class DiscourseConfig
       class << self
         def call
-          check_credentials
+          check_discourse_credentials
+          check_vault_credentials
         end
 
         private
 
-        def check_credentials
+        def check_discourse_credentials
           discourse_username = Discourse::Config.get('credentials', 'discourse_username')
 
           return if discourse_username
 
           discourse_username = CLI::UI::Prompt.ask("What's your Discourse username?")
           Discourse::Config.set('credentials', 'discourse_username', discourse_username)
+        end
+
+        def check_vault_credentials
+          vault_dir = Discourse::Config.get('vault', 'vault_dir')
+
+          return if vault_dir
+
+          vault_dir = CLI::UI::Prompt.ask('What directory is your Obsidian Vault in?')
+          Discourse::Config.set('vault', 'vault_dir', vault_dir)
         end
       end
     end
