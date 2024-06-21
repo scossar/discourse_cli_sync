d9a12375ac38614cffbd76475825230722561095
scossar <scossar3@gmail.com>
2024-06-20 19:17:24 -0700

    cleanup

--- test/discourse_config_test.rb
+++ test/discourse_config_test.rb
@@ -1,18 +1,34 @@
 # frozen_string_literal: true
 
 require_relative 'test_helper'
+require_relative '../lib/utils/discourse_config'
 
 module Discourse
   module Utils
     class DiscourseConfigTest < Minitest::Test
       include CLI::Kit::Support::TestHelper
-      include CLI::Kit::Support::TestHelper::FakeConfig
+      include FakeConfig
 
-      # Leaving this in place for reference
-      def test_fake_config
-        Discourse::Config.set('credentials', 'discourse_username', 'foo')
-        discourse_username = Discourse::Config.get('credentials', 'discourse_username')
-        assert_equal discourse_username, 'foo', 'The discourse_username should be set to foo'
+      def setup
+        super
+        Discourse::Config.set('credentials', 'discourse_username', nil)
+      end
+
+      def test_username_not_set
+        CLI::UI::Prompt.expects(:ask).with("What's your Discourse username?").returns('scossar')
+
+        Discourse::Utils::DiscourseConfig.call
+
+        assert_equal 'scossar', Discourse::Config.get('credentials', 'discourse_username')
+      end
+
+      def test_username_set
+        Discourse::Config.set('credentials', 'discourse_username', 'scossar')
+        CLI::UI::Prompt.expects(:ask).with("What's your Discourse username?").never
+
+        Discourse::Utils::DiscourseConfig.call
+
+        assert_equal 'scossar', Discourse::Config.get('credentials', 'discourse_username')
       end
     end
   end
