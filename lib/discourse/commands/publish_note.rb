643ddc06670e0e1e6d2e3e44db832f17a4d70b0d
scossar <scossar3@gmail.com>
2024-06-20 23:11:02 -0700

    add api_credentials module

--- lib/discourse/commands/publish_note.rb
+++ lib/discourse/commands/publish_note.rb
@@ -1,12 +1,14 @@
 # frozen_string_literal: true
 
 require_relative '../../utils/discourse_config'
+require_relative '../../utils/api_credentials'
 
 module Discourse
   module Commands
     class PublishNote < Discourse::Command
       def call(_args, _name)
         Discourse::Utils::DiscourseConfig.call
+        Discourse::Utils::ApiCredentials.call
       end
 
       def self.help
---
+++ lib/utils/api_credentials.rb
@@ -0,0 +1,31 @@
+# frozen_string_literal: true
+
+module Discourse
+  module Utils
+    class ApiCredentials
+      class << self
+        def call
+          api_key
+        end
+
+        private
+
+        def api_key
+          encrypted_key = Discourse::Config.get('credentials', 'encrypted_api_key')
+
+          unless encrypted_key
+            loop do
+              encrypted_key = CLI::UI::Prompt.ask_password('API Key')
+              confirm = CLI::UI::Prompt.confirm('Was that correct?')
+
+              break if confirm
+            end
+          end
+          # call some function that encrypts the key
+
+          Discourse::Config.set('credentials', 'encrypted_api_key', encrypted_key)
+        end
+      end
+    end
+  end
+end
