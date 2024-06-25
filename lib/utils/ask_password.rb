dad0a70b709c63bf0a5a229f2e94801d1eb7bb62
scossar <scossar3@gmail.com>
2024-06-25 13:02:22 -0700

    add AskPassword module

---
+++ lib/utils/ask_password.rb
@@ -0,0 +1,22 @@
+# frozen_string_literal: true
+
+module Discourse
+  module Utils
+    module AskPassword
+      def self.ask_password(prompt)
+        CLI::UI::Prompt.ask_password(prompt)
+      end
+
+      def self.ask_and_confirm_password(prompt, confirm_prompt)
+        password = nil
+        loop do
+          password = CLI::UI::Prompt.ask_password(prompt)
+          password_confirm = CLI::UI::Prompt.ask_password(confirm_prompt)
+          break if password == password_confirm
+
+          CLI::UI::Format "The passwords didn't match. Please try again."
+        end
+      end
+    end
+  end
+end
