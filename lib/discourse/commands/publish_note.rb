4abe6a4a16127951a25606fafb2187c547231f9d
scossar <scossar3@gmail.com>
2024-06-27 00:36:38 -0700

    get api_key and password

--- lib/discourse/commands/publish_note.rb
+++ lib/discourse/commands/publish_note.rb
@@ -1,18 +1,32 @@
 # frozen_string_literal: true
 
-require_relative '../../utils/discourse_config'
 require_relative '../../utils/api_credentials'
+require_relative '../../utils/api_key'
+require_relative '../../utils/ask_password'
+require_relative '../../utils/discourse_config'
 
 module Discourse
   module Commands
     class PublishNote < Discourse::Command
       def call(_args, _name)
-        Discourse::Utils::DiscourseConfig.call
-        Discourse::Utils::ApiCredentials.call
+        password, api_key = credential_frames
+        puts "password: #{password}, api_key: #{api_key}"
       end
 
       def self.help
         'Publishes a markdown file to Discourse'
+      end
+
+      def credential_frames
+        CLI::UI::Frame.open('Discourse credentials') do
+          Discourse::Utils::DiscourseConfig.call
+          # TODO: return the password from ApiCredentials.call.
+          # if it's not null, don't ask again in the command
+          Discourse::Utils::ApiCredentials.call
+          password = Discourse::Utils::AskPassword.ask_password('Your API key password')
+          api_key = Discourse::Utils::ApiKey.api_key(password)
+          [password, api_key]
+        end
       end
     end
   end
