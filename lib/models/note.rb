b285e50b8310ca9e0a15f7c533006ac485a29bdb
scossar <scossar3@gmail.com>
2024-06-29 16:45:29 -0700

    add validations

--- lib/models/note.rb
+++ lib/models/note.rb
@@ -2,10 +2,13 @@
 
 module Discourse
   class Note < ActiveRecord::Base
-    belongs_to :directory
-    belongs_to :discourse_category
+    belongs_to :directory, optional: true
+    belongs_to :discourse_category, optional: true
 
     validates :title, presence: true, uniqueness: { scope: :directory_id }
     validates :local_only, inclusion: { in: [true, false] }
+    validates :topic_url, uniqueness: true
+    validates :topic_id, uniqueness: true
+    validates :post_id, uniqueness: true
   end
 end
