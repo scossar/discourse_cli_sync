2704b9236db92cce9246eb0c4c4493787b16408f
scossar <scossar3@gmail.com>
2024-06-29 17:54:13 -0700

    refactor

--- lib/utils/category_info.rb
+++ lib/utils/category_info.rb
@@ -31,26 +31,29 @@ module Discourse
       def self.category_info(categories)
         categories.each_value do |category|
           CLI::UI::Frame.open(category[:name]) do
-            spin_group = CLI::UI::SpinGroup.new
-            spin_group.failure_debrief do |_title, exception|
-              puts CLI::UI.fmt "  #{exception}"
-            end
-
-            spin_group.add('Updating Categories') do |spinner|
-              Discourse::DiscourseCategory
-                .create_or_update(name: category[:name],
-                                  slug: category[:slug],
-                                  read_restricted: category[:read_restricted],
-                                  description: category[:description_excerpt])
-              spinner.update_title("Updated info for #{category[:name]}")
-            end
-
-            spin_group.wait
-
+            create_or_update_category(category)
             puts CLI::UI.fmt "read_restricted: #{category[:read_restricted]}"
             puts CLI::UI.fmt category[:description_excerpt]
           end
         end
+      end
+
+      def self.create_or_update_category(category)
+        spin_group = CLI::UI::SpinGroup.new
+        spin_group.failure_debrief do |_title, exception|
+          puts CLI::UI.fmt "  #{exception}"
+        end
+
+        spin_group.add('Updating Categories') do |spinner|
+          Discourse::DiscourseCategory
+            .create_or_update(name: category[:name],
+                              slug: category[:slug],
+                              read_restricted: category[:read_restricted],
+                              description: category[:description_excerpt])
+          spinner.update_title("Updated info for #{category[:name]}")
+        end
+
+        spin_group.wait
       end
     end
   end
