edd6d2de43da8e11be0ad5f0dbd81adae7169756
scossar <scossar3@gmail.com>
2024-07-02 00:32:17 -0700

    move credential_frame to module

--- lib/discourse/commands/publish_directory.rb
+++ lib/discourse/commands/publish_directory.rb
@@ -1,30 +1,16 @@
 # frozen_string_literal: true
 
-require_relative '../../utils/api_credentials'
-require_relative '../../utils/api_key'
-require_relative '../../utils/ask_password'
-require_relative '../../utils/configure_host'
-
+require_relative '../../utils/credential_frame'
 module Discourse
   module Commands
     class PublishDirectory < Discourse::Command
       def call(_args, _name)
-        discourse_site, api_key = credential_frames
+        discourse_site, api_key = Discourse::Utils::CredentialFrame.credentials_for_site
+        puts "site: #{discourse_site}, api_key: #{api_key}"
       end
 
       def self.help
         'Publishes a vault directory to Discourse'
-      end
-
-      def credential_frames
-        CLI::UI::Frame.open('Discourse credentials') do
-          discourse_site = Discourse::Utils::ConfigureHost.call
-          password = Discourse::Utils::ApiCredentials.call(discourse_site)
-          password ||= Discourse::Utils::AskPassword.ask_password('Your API key password')
-          api_key = Discourse::Utils::ApiKey.api_key(discourse_site, password)
-          puts "api key: #{api_key}"
-          [discourse_site, api_key]
-        end
       end
     end
   end
---
+++ lib/utils/credential_frame.rb
@@ -0,0 +1,22 @@
+# frozen_string_literal: true
+
+require_relative 'api_credentials'
+require_relative 'api_key'
+require_relative 'ask_password'
+require_relative 'configure_host'
+
+module Discourse
+  module Utils
+    module CredentialFrame
+      def self.credentials_for_site
+        CLI::UI::Frame.open('Discourse credentials') do
+          discourse_site = Discourse::Utils::ConfigureHost.call
+          password = Discourse::Utils::ApiCredentials.call(discourse_site)
+          password ||= Discourse::Utils::AskPassword.ask_password('Your API key password')
+          api_key = Discourse::Utils::ApiKey.api_key(discourse_site, password)
+          [discourse_site, api_key]
+        end
+      end
+    end
+  end
+end
