bec137819be8e445d9ac4d8c5568d0be292f87d5
scossar <scossar3@gmail.com>
2024-07-03 16:00:39 -0700

    update Note model - a Note belongs to a Directory

--- lib/models/note.rb
+++ lib/models/note.rb
@@ -2,15 +2,13 @@
 
 module Discourse
   class Note < ActiveRecord::Base
-    belongs_to :discourse_site
     belongs_to :directory
-    belongs_to :discourse_category, optional: true
 
     validates :title, presence: true, uniqueness: { scope: %i[directory_id discourse_site_id] }
     validates :local_only, inclusion: { in: [true, false] }
     validates :topic_url, uniqueness: true
     validates :topic_id, uniqueness: true
     validates :post_id, uniqueness: true
-    validates :discourse_site, presence: true
+    validates :discourse_directory, presence: true
   end
 end
