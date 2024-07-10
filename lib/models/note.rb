077d3a93c640a6a1573221fcb028bf8881199292
scossar <scossar3@gmail.com>
2024-07-10 13:32:37 -0700

    don't overwrite set fields with nil

--- lib/models/note.rb
+++ lib/models/note.rb
@@ -13,7 +13,7 @@ module Discourse
     validates :directory, presence: true
     validates :discourse_site, presence: true
 
-    def self.create(params)
+    def self.create_note(params)
       title = params[:title]
       local_only = params[:local_only] || false
       topic_url = params[:topic_url]
@@ -24,29 +24,19 @@ module Discourse
       file_id = params[:file_id]
 
       Note.create(title:, local_only:, topic_url:, topic_id:, post_id:, discourse_site:,
-                  directory:, file_id:).tap do |result|
-        raise Discourse::Errors::BaseError, 'Unable to create Note' unless result.persisted?
+                  directory:, file_id:).tap do |note|
+        raise Discourse::Errors::BaseError, 'Unable to create Note' unless note.persisted?
       end
     rescue StandardError => e
       raise Discourse::Errors::BaseError, "Error creating Note: #{e.message}"
     end
 
-    def self.update(note, params = {})
-      title = params[:title]
-      local_only = params[:local_only] || false
-      topic_url = params[:topic_url]
-      topic_id = params[:topic_id]
-      post_id = params[:post_id]
-      discourse_site = params[:discourse_site]
-      directory = params[:directory]
-      file_id = params[:file_id]
-
-      note.update(title:, local_only:, topic_url:, topic_id:, post_id:, discourse_site:,
-                  directory:, file_id:).tap do |result|
+    def self.update_note(note, params = {})
+      note.update(params).tap do |result|
         raise Discourse::Errors::BaseError, 'Error updating Note' unless result
       end
     rescue StandardError => e
-      rails Discourse::Errors::BaseError, "Error updating Note: #{e.message}"
+      raise Discourse::Errors::BaseError, "Error updating Note: #{e.message}"
     end
   end
 end
