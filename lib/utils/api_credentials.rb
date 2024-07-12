c6d28743cd642edc19701b34630d04d8374671b2
scossar <scossar3@gmail.com>
2024-07-11 22:23:24 -0700

    allow api_key and password to be changed

--- lib/utils/api_credentials.rb
+++ lib/utils/api_credentials.rb
@@ -11,8 +11,9 @@ module Discourse
     class ApiCredentials
       class << self
         def call(discourse_site)
-          domain = discourse_site.domain
-          credentials_for_site(discourse_site, domain)
+          @discourse_site = discourse_site
+          @domain = @discourse_site.domain
+          credentials_for_site
         end
 
         def ask_password_prompt
@@ -35,35 +36,51 @@ module Discourse
           "Confirm that the key starting with #{truncated_key} is correct for #{domain}"
         end
 
-        def credentials(discourse_site)
-          [discourse_site&.iv, discourse_site&.salt, discourse_site&.encrypted_api_key]
+        def credentials
+          [@discourse_site&.iv, @discourse_site&.salt, @discourse_site&.encrypted_api_key]
         end
 
-        def credentials_for_site(discourse_site, domain)
-          iv, salt, encrypted_api_key = credentials(discourse_site)
+        def credentials_for_site
+          iv, salt, encrypted_api_key = credentials
 
           if iv && salt && encrypted_api_key
-            puts CLI::UI.fmt "Api credentials are configured for #{domain}"
-            nil
+            puts CLI::UI.fmt "Api credentials are configured for #{@domain}"
+            update_credentials = CLI::UI::Prompt.confirm('Would you like to update the API Key ' \
+                                                         "and password for #{@domain}?")
+            confirm = nil
+            if update_credentials
+              confirm = CLI::UI::Prompt
+                        .confirm('Confirm you would like to update your API credentials')
+            end
+            return nil unless update_credentials && confirm
+
+            update_api_key
           else
             password = AskPassword.ask_and_confirm_password(ask_password_prompt,
                                                             password_confirm_prompt,
                                                             password_mismatch_prompt)
-            set_api_key(discourse_site:, domain:, password:)
+            set_api_key(password:)
             password
           end
         end
 
-        def set_api_key(discourse_site:, domain:, password:)
+        def update_api_key
+          password = AskPassword.ask_and_confirm_password(ask_password_prompt,
+                                                          password_confirm_prompt,
+                                                          password_mismatch_prompt)
+          set_api_key(password:)
+        end
+
+        def set_api_key(password:)
           unencrypted_key, key_start = nil
           loop do
-            unencrypted_key = ask_password(api_key_prompt(domain))
+            unencrypted_key = ask_password(api_key_prompt(@domain))
             key_start = unencrypted_key[0, 4]
-            confirm = confirm(api_key_confirm_prompt(domain, key_start))
+            confirm = confirm(api_key_confirm_prompt(@domain, key_start))
             break if confirm
           end
           iv, salt, encrypted_api_key = Encryption.encrypt(password, unencrypted_key)
-          discourse_site.update(iv:, salt:, encrypted_api_key:)
+          @discourse_site.update(iv:, salt:, encrypted_api_key:)
           password
         end
 
