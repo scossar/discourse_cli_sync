3f65337ebe7e600a0c254b57fc936afa65ebf999
scossar <scossar3@gmail.com>
2024-07-12 20:45:06 -0700

    changes for rubocop

--- lib/discourse/commands/help.rb
+++ lib/discourse/commands/help.rb
@@ -1,19 +1,22 @@
+# frozen_string_literal: true
+
 require 'discourse'
 
 module Discourse
   module Commands
     class Help < Discourse::Command
-      def call(args, _name)
-        puts CLI::UI.fmt("{{bold:Available commands}}")
-        puts ""
+      def call(_args, _name)
+        puts CLI::UI.fmt('{{bold:Available commands}}')
+        puts ''
 
         Discourse::Commands::Registry.resolved_commands.each do |name, klass|
           next if name == 'help'
+
           puts CLI::UI.fmt("{{command:#{Discourse::TOOL_NAME} #{name}}}")
-          if help = klass.help
+          if (help = klass.help)
             puts CLI::UI.fmt(help)
           end
-          puts ""
+          puts ''
         end
       end
     end
